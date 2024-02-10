4664c43bbf8bfa386684a80858229fb29ab06d99
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 15:34:16 +1300

    WIP

--- src/Models/BlockElementList.php
+++ src/Models/BlockElementList.php
@@ -6,11 +6,11 @@ use Goldfinch\Fielder\Fielder;
 use Goldfinch\Mill\Traits\Millable;
 use Goldfinch\Fielder\Traits\FielderTrait;
 use DNADesign\ElementalList\Model\ElementList;
-use Goldfinch\Helpers\Traits\BaseElementTrait;
+use Goldfinch\Helpers\Traits\ElementListTrait;
 
 class BlockElementList extends ElementList
 {
-    use FielderTrait, Millable, BaseElementTrait;
+    use FielderTrait, Millable, ElementListTrait;
 
     public function fielder(Fielder $fielder): void
     {
