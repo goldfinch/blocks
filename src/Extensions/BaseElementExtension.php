1a22f8b36748745ad5692e90e99c5d0fd455ee91
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-04-18 08:22:21 +1200

    WIP

---
+++ src/Extensions/BaseElementExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Goldfinch\Blocks\Extensions;
+
+use SilverStripe\Forms\FieldList;
+use Goldfinch\Blocks\Pages\Blocks;
+use SilverStripe\ORM\DataExtension;
+
+class BaseElementExtension extends DataExtension
+{
+    public function updatePreviewLink($link)
+    {
+        $page = $this->owner->getPage();
+
+        if ($page && $page->ClassName == Blocks::class) {
+            $link = null; // $page->AbsoluteLink() . $link;
+        }
+    }
+}
